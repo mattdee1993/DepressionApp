6ae7774a74075c2a175f21387c9208bce2df25a7
Matthew Dee <Matthew@matthews-macbook-pro.local>
2016-03-08 16:48:35 +0000

    Initial Commit

---
+++ DepressionApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DepressionApp
+//
+//  Created by Matthew Dee on 08/03/2016.
+//  Copyright © 2016 Matthew Dee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
